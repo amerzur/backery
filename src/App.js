2118416c60fcc8ac97b898d33d9499e6dfcb7eca
Amer <amerzreigat@amers-MBP.fritz.box>
2021-01-22 11:35:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from './axios-orders'
+ import {useState,Fragment} from 'react'
 function App() {
+
+  const [dark, toggleDarkMode] = useState(false);
+  const [falling, fallMailBox] = useState(false);
+
+ function doorClickedHanlder(e)  {
+   if(dark){
+     alert('Oops ! sorry we are closed now !')
+   }else{
+     axios.post('/')
+   .then(function(res){
+    alert('order made!'+res.data.id);
+   })
+   .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+   }
+   
+  }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+           {dark?<div class="twinkling"></div>:''    }
+           {dark?<div class="stars"></div>:''    }
+
+    <div className={"App-header d-flex" + (dark ? "  night":" day")} >
+           <div className="frame d-flex flex-column">
+   
+          <div  className={dark?"moon":"sun"} onClick={()=>toggleDarkMode(!dark)}></div> 
+        <div id="triangle-up">
+        </div>
+        <div id="square" className="d-flex justify-content-around flex-column">
+        <div id="label" className="mx-auto">COOKIE BAKERY</div>
+
+          <div className="d-flex flex-row justify-content-around">
+          <div className={"window "+ (dark ? " night":" day")}></div>
+          <div className={"window "+ (dark ? " night":" day")}></div>
+          </div>
+       
+          <div className="d-flex flex-row justify-content-around">
+          <div id="door" className="mx-auto" onClick={()=>doorClickedHanlder()}><div id="door-hand"></div></div>
+          <div className="mail-container">
+                    <div id="mail-box" className={falling?'falling':''} onClick={()=>fallMailBox(true)}>
+                      <div id="mail-box-header"></div>
+                      Mailbox
+                    </div>
+                    
+                    </div>
+          </div>
+          
+        </div>
+        </div>
+     </div>
+     </Fragment>
   );
 }
 
